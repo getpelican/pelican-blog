0762ecf974aa2555a4a036138c70a9e985591c75
Justin Mayer <entroP@gmail.com>
2013-05-31 07:41:44 -0700

    Add Twitter and GitHub links

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,8 +13,12 @@ LOCALE = 'en_US.UTF-8'
 MENUITEMS = (('documentation', 'http://docs.getpelican.com'), )
 
 # Blogroll
-LINKS = (('Pelican', 'http://docs.getpelican.com'),
-         ('Python.org', 'http://python.org'),
-         ('Jinja2', 'http://jinja.pocoo.org'))
+LINKS = (('Pelican', 'http://docs.getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'))
+
+SOCIAL = (('twitter', 'http://twitter.com/getpelican'),
+          ('github', 'https://github.com/getpelican'),
+         )
 
 DEFAULT_PAGINATION = 5
