1bc99b613f1be9921404f05753888959c4366249
Justin Mayer <entroP@gmail.com>
2022-06-22 12:33:27 +0200

    Improve publish settings

--- publishconf.py
+++ publishconf.py
@@ -1,20 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-
 import sys
-sys.path.append('.')
-from pelicanconf import *
+sys.path.append(".")
+from pelicanconf import *  # noqa
 
-SITEURL = 'http://blog.getpelican.com'
+SITEURL = "https://blog.getpelican.com"
 
 DELETE_OUTPUT_DIRECTORY = True
-
-GAUGES = "516ffc13613f5d0d63000002"
-
-# Following items are often useful when publishing
-
-# Uncomment following line for absolute URLs in production:
-#RELATIVE_URLS = False
-
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
