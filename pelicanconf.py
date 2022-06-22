c04197f77e735e44a261f5030295e088f88a62ed
Justin Mayer <entroP@gmail.com>
2022-06-22 12:37:13 +0200

    Update home page links

--- pelicanconf.py
+++ pelicanconf.py
@@ -17,11 +17,13 @@ MENUITEMS = (('documentation', 'https://docs.getpelican.com'),
 DISPLAY_PAGES_ON_MENU = False
 
 # Blogroll
-LINKS = (('Pelican', 'http://docs.getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'))
+SOCIAL_WIDGET_NAME = "follow"
+LINKS = (('Pelican Docs', 'https://docs.getpelican.com/'),
+         ('Support Pelican', 'https://donate.getpelican.com/'),
+         ('Justin Mayer', 'https://justinmayer.com/'))
 
-SOCIAL = (('twitter', 'http://twitter.com/getpelican'),
+SOCIAL = (('@getpelican', 'https://twitter.com/getpelican'),
+          ('@jmayer', 'https://twitter.com/jmayer'),
           ('github', 'https://github.com/getpelican'),
          )
 
