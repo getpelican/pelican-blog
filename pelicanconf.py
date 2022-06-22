806826233bd969980760dfad97b565db43d964df
Justin Mayer <entroP@gmail.com>
2022-06-22 13:14:28 +0200

    Standardize on double-quotation marks in settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -9,27 +9,31 @@ PATH = "content"
 CACHE_CONTENT = False
 LOAD_CONTENT_CACHE = False
 
-TIMEZONE = 'Europe/Rome'
+TIMEZONE = "Europe/Rome"
 
-DEFAULT_LANG = 'en'
-LOCALE = 'en_US.UTF-8'
+DEFAULT_LANG = "en"
+LOCALE = "en_US.UTF-8"
 
-MENUITEMS = (('documentation', 'https://docs.getpelican.com'),
-             ('contribute', 'https://donate.getpelican.com'),
-             ('gratitude', '/pages/gratitude.html'),
-            )
+MENUITEMS = (
+    ("documentation", "https://docs.getpelican.com"),
+    ("contribute", "https://donate.getpelican.com"),
+    ("gratitude", "/pages/gratitude.html"),
+)
 DISPLAY_PAGES_ON_MENU = False
 
 # Blogroll
 SOCIAL_WIDGET_NAME = "follow"
-LINKS = (('Pelican Docs', 'https://docs.getpelican.com/'),
-         ('Support Pelican', 'https://donate.getpelican.com/'),
-         ('Justin Mayer', 'https://justinmayer.com/'))
+LINKS = (
+    ("Pelican Docs", "https://docs.getpelican.com/"),
+    ("Support Pelican", "https://donate.getpelican.com/"),
+    ("Justin Mayer", "https://justinmayer.com/"),
+)
 
-SOCIAL = (('@getpelican', 'https://twitter.com/getpelican'),
-          ('@jmayer', 'https://twitter.com/jmayer'),
-          ('github', 'https://github.com/getpelican'),
-         )
+SOCIAL = (
+    ("@getpelican", "https://twitter.com/getpelican"),
+    ("@jmayer", "https://twitter.com/jmayer"),
+    ("github", "https://github.com/getpelican"),
+)
 
 DEFAULT_PAGINATION = 5
 
